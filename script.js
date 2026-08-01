f89c45f3da248a82be4f6e813b7fc379f6e665df
lostvalkrie <lostvalkrie08@gmail.com>
2026-08-01 21:34:46 +0300

    Complete task 1

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+console.log("WHILE");
+let whileI = 25;
+while (whileI >= 0) {
+  console.log(whileI);
+  whileI--;
+}
+
+console.log("DO WHILE");
+let doWhileI = 25;
+do {
+  console.log(doWhileI);
+  doWhileI--;
+} while (doWhileI >= 0);
+
+console.log("FOR");
+for (let i = 25; i >= 0; i--) {
+  console.log(i);
+}
